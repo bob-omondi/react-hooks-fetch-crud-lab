ab1e398f92065aaeb98e14f68061ea076a7ebc51
+ <+>
2022-10-04 23:01:25 +0300

    complete

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,49 @@
-import React from "react";
+import React, { useState,useEffect } from "react";
+import QuestionItem from "./QuestionItem";
 
 function QuestionList() {
+  const[questions, setQuestions] = useState([])
+  
+  useEffect(() => {
+    fetch("http://localhost:4000/questions")
+    .then(r => r.json())
+    .then(data => setQuestions(data))
+    
+    
+  },[])
+
+ function handleDeleteQuestion(deletedQuestion){
+ const updatedQuestions = questions.filter(question => question.id !== deletedQuestion.id)
+ setQuestions(updatedQuestions)
+
+ }
+
+ function handleAnswerChange(changedQuestion){
+
+  const updatedQuestions = questions.map(question => {
+    if(question.id === changedQuestion.id){
+      return changedQuestion
+    }else{
+      return question
+    }
+  })
+   setQuestions(updatedQuestions)
+ }
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>
+      {questions.map(question => {
+        return (
+          <QuestionItem
+          key={question.id}
+          question={question}
+          onDelete={handleDeleteQuestion}
+          onAnswerChange={handleAnswerChange} />
+        )
+      })}
+        {/* display QuestionItem components here after fetching */}</ul>
     </section>
   );
 }
